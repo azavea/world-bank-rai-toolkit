07fa54048cc45dea901ee3f04947c43b2ee0974e
ahassan@azavea.com <adeel.hassan93@gmail.com>
2021-06-17 17:32:10 +0500

    add CustomGeocoder

--- rai/utils.py
+++ rai/utils.py
@@ -1,7 +1,14 @@
-from typing import Optional, Union
+from typing import Dict, Optional, Union, List, Any
+import os
 import unicodedata as ud
+from collections import defaultdict
+
 from pyproj import CRS
 from shapely.geometry import Point, LineString
+
+from tqdm import tqdm
+import pandas as pd
+import geopandas as gpd
 
 GEOD = CRS.from_epsg(4326).get_geod()
 
@@ -23,7 +30,9 @@ def rmdiacritics(char):
     return char
 
 
-def remove_diacritics(s: str):
+def remove_diacritics(s: Any) -> Any:
+    if not isinstance(s, str):
+        return s
     return ''.join(map(rmdiacritics, s))
 
 
@@ -36,3 +45,50 @@ def straight_line_distance(p1: Point,
     else:
         dist = GEOD.geometry_length(line_string)
     return dist
+
+
+def read_geonames_csv(path: os.PathLike) -> pd.DataFrame:
+    column_names = [
+        'geonameid', 'name', 'asciiname', 'alternatenames', 'latitude',
+        'longitude', 'feature class', 'feature code', 'country code', 'cc2',
+        'admin1 code', 'admin2 code', 'admin3 code', 'admin4 code',
+        'population', 'elevation', 'dem', 'timezone', 'modification '
+    ]
+    df = pd.read_csv(path, delimiter='\t', header=None)
+    df.columns = column_names
+    df.loc[:, 'orig_name'] = df.name
+    df.name = df.asciiname.str.lower()
+    df.alternatenames = df.alternatenames.str.lower().map(remove_diacritics)
+    df.longitude = df.longitude.astype(float)
+    df.latitude = df.latitude.astype(float)
+    gdf = gpd.GeoDataFrame(
+        df, geometry=gpd.points_from_xy(df.longitude, df.latitude))
+    return gdf
+
+
+def geonames_to_dict(places_df: gpd.GeoDataFrame) -> Dict[str, List[Point]]:
+    places = places_df.name.to_numpy()
+    places_set = set(places)
+    places_to_geoms = defaultdict(list)
+
+    with tqdm(places_set, desc='Mapping names to points') as bar:
+        for p in bar:
+            poitns = places_df.loc[places == p, 'geometry'].to_list()
+            places_to_geoms[p] += poitns
+    places_to_alts = defaultdict(set)
+
+    with tqdm(places_set, desc='Mapping names to alternate names') as bar:
+        for p in bar:
+            alts = places_df.loc[places == p, 'alternatenames']
+            alts = [v for v in alts if isinstance(v, str)]
+            if len(alts) > 0:
+                alts = set.union(*[set(v.split(',')) for v in alts])
+                places_to_alts[p] = places_to_alts[p].union(alts)
+    with tqdm(
+            list(places_to_geoms.items()),
+            desc='Mapping alternate names to points') as bar:
+        for p, g in bar:
+            alts = places_to_alts[p]
+            for alt in alts:
+                places_to_geoms[alt] += g
+    return places_to_geoms
